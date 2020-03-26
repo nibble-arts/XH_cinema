4f5de7629ad2c7883d415ea2b6da9b87aed10bd7
tom@home <thomas.winkler@iggmp.net>
2020-03-26 15:43:25 +0100

    adding fullscreen mode to player

--- classes/view.php
+++ classes/view.php
@@ -28,23 +28,30 @@ class View {
 
 	public static function host($name) {
 
+		$o .= '';
+
 		// TITLE
-		$o = '<div class="cinema_host_title">';
-			$o .= Text::host_title();
-		$o .= '</div>';
+		// $o .= '<div id="cinema_fullscreen">';
 
-		// player
-		$o .= '<div id="cinema_host_wrapper">';
-			// $o .= '<div class="cinema_host_wrapper">';
-				$o .= '<div class="cinema_host_wrapper" id="twitch-embed"></div>';
-			// $o .= '</div>';
-		$o .= '</div>';
+			$o .= '<div class="cinema_host_title">';
+				$o .= Text::host_title();
+			$o .= '</div>';
 
-		// $o .= '<div class="cinema_chat_count"></div>';
-		$o .= '<div class="cinema_chat_users"></div>';
+			// player
+			$o .= '<div id="cinema_host_wrapper">';
+				// $o .= '<div class="cinema_host_wrapper">';
+					$o .= '<div class="cinema_host_wrapper" id="twitch-embed"></div>';
+				// $o .= '</div>';
+			$o .= '</div>';
 
-		// CHAT section
-		$o .= '<div id="cinema_chat" type="inline"></div>';
+			// $o .= '<div class="cinema_chat_count"></div>';
+			$o .= '<div class="cinema_chat_users"></div>';
+
+			// CHAT section
+			$o .= '<div id="cinema_chat" type="inline"></div>';
+
+		// $o .= '</div>';
+
 		return $o;
 	}
 
